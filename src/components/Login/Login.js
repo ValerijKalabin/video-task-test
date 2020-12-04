9a06d34fbc05f2dbde73bb285641cbc93377a004
Valerij Kalabin <valerijkalabin@yandex.ru>
2020-12-04 16:42:06 +0700

    feature: create login function

---
+++ src/components/Login/Login.js
@@ -0,0 +1,32 @@
+import './Login.css';
+import logo from '../../images/logo.svg';
+import { Form, Input, Button } from 'antd';
+
+function Login() {
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <Form
+      layout="vertical"
+      name="basic"
+      className="login__form"
+      onFinish={onFinish}
+    >
+      <img className="login__logo" src={logo} alt="Логотип" />
+      <h1 className="login__title">Вход</h1>
+      <Form.Item label="Логин" name="username">
+        <Input className="login__input" />
+      </Form.Item>
+      <Form.Item label="Пароль" name="password">
+        <Input.Password className="login__input"/>
+      </Form.Item>
+      <Form.Item>
+        <Button className="login__button" htmlType="submit" type="primary">Войти</Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default Login;
